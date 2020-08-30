b1ef64da94762c92b9c9d51b7194ec4626bd6b38
unknown <serjumano17@gmail.com>
2020-08-30 16:48:45 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <h1>ZIRO APP</h1>
+    </>
+  )
+}
+
+export default hot(module)(App)
---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+}
